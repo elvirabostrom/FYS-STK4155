481a7e065a5ba8aecb8937519c9917719c4b5a10
ElviraBostrom <elvirahbostrom@gmail.com>
2026-09-08 21:42:08 +0200

    Clean up comments

--- project1/run_closed_form.py
+++ project1/run_closed_form.py
@@ -7,14 +7,6 @@ num_points = np.array((50, 100, 250, 500))
 noise = 0.1
 degrees = np.arange(1, 16, 1) # MÅ 0 VÆRE MED? ISF MÅ INTERCEPT TILBAKE
 
-# skalerer ikke OLS pga intervallet x ligger i gjør at høye potenser faktisk blir mindre eller holder seg
-# i samme størrelsesorden
-# OLS vil gi samme prediksjon med og uten skalering
-# SKALERER ALLIKEVEL fordi Runge funksjonen kan kreve høye polynomgrader og da blir kolonnene i designmatrise
-# sterkt korrelerte. Kan få en ill-conditioned matrise etter hvert. 
-# skalering krymper tallverdiene til samme statistiske skala, som gjør matriseberegninger stabile for maskinen
-# DERMED kan vi være trygge på at eventuelle forskjeller i MSE kommer av algoritmene selv
-
 
 # OLS, calculate and save results for given noise, varying n and polynomial degree
 for n in num_points:
